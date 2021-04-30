bb16931746a8c6c16ca602697726983b2f8da3f3
cspraggett <cspraggett@gmail.com>
2021-04-30 14:25:59 -0400

    First draft

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function checkPrincipal() {
+	principal = document.getElementById('principal');
+	if (principal.value <= 0) {
+		alert('Enter a positive number');
+		principal.focus();
+	}
 }
-        
+
+function updateRate() {
+	let rateval = document.getElementById('rate').value;
+	document.getElementById('rate_val').innerText = rateval + '%';
+}
+
+function compute() {
+	let principal = document.getElementById('principal').value;
+	let rate = document.getElementById('rate').value;
+	let years = document.getElementById('years').value;
+	let interest = (principal * years * rate) / 100;
+	let year = new Date().getFullYear() + parseInt(years);
+
+	checkPrincipal();
+	document.getElementById(
+		'result'
+	).innerHTML = `If you deposit \<mark\>${principal}\</mark\>,\<br\>at an interest rate of \<mark\>${rate}%\</mark\>\<br\>You will receive an amount of \<mark\>${interest}\</mark\>,\<br\>in the year \<mark\>${year}\</mark\>\<br\>`;
+}
